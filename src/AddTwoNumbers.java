ea4d16779ebc6b8f73127002c157b062daa9e6ad
SupRoya <rarkhmammadova22@ku.edu.tr>
2024-03-04 17:23:47 +0300

    2

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,36 @@
+public class AddTwoNumbers {
+
+//      Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+          ListNode useless = new ListNode();
+          ListNode current = useless;
+          int carry = 0;
+
+          while (l1 != null || l2 !=null || carry > 0) {
+              if (l1 != null) {
+                  carry += l1.val;
+                  l1 = l1.next;
+              }
+
+              if (l2 != null) {
+                  carry += l2.val;
+                  l2 = l2.next;
+              }
+
+              current.next = new ListNode(carry % 10);
+              carry /= 10;
+              current = current.next;
+          }
+
+          return useless.next;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+//    public static void test1() {
+//        TwoSum a = new TwoSum();
+//        int[] test = new int[]{2,7,11,15};
+//        int target = 9;
+//        int[] produced_result = a.twoSum(test, target);
+//        int[] expected = new int[]{0,1};
+//
+//        if (produced_result == expected) {
+//            System.out.println("test 1: pass");
+//        } else {
+//            System.out.println("test 1: fail");
+//        }
+//    }
+//
+//    public static void test2() {
+//        TwoSum a = new TwoSum();
+//        int[] test = new int[]{3,2,4};
+//        int target = 6;
+//        int[] produced_result = a.twoSum(test, target);
+//        int[] expected = new int[]{1,2};
+//
+//        if (produced_result == expected) {
+//            System.out.println("test 2: pass");
+//        } else {
+//            System.out.println("test 2: fail");
+//        }
+//    }
 }
+
+
